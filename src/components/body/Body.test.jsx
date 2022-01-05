49f8aff09a38db6f07879922d98e838505e406d0
TANNER MECK <steve.meck246@gmail.com>
2022-01-05 14:55:26 -0800

    all tests passing

--- src/components/body/Body.test.jsx
+++ src/components/body/Body.test.jsx
@@ -1,51 +1,43 @@
 import {screen, render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event'
-import { GuestProvider } from '../../context/GuestContext';
+import userEvent from '@testing-library/user-event';
+import { GuestProvider } from '../../context/__Mocks__/GuestContext';
 import Body from './Body';
-// import { MemoryRouter, Switch, Route } from 'react-router-dom';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
 import Login from '../login/Login';
 
-// jest.mock('../../context/GuestContext', () => {
-//     return {
-//         useGuest: function () {
-//             return {
-//                 guest: 'tanner'
-//             }
-//         }
-//     }
-// });
+jest.mock('../../context/GuestContext', () => {
+    return {
+        useGuest: function () {
+            return {
+                guest: 'tanner'
+            }
+        }
+    }
+});
 
-// it('should render the body component', async() => {
-//     const { container } = render(
-//                             <GuestProvider>
-//                                 <MemoryRouter>
-//                                     <Switch>
-//                                         <Route exact path='/'>
-//                                             <Body />
-//                                         </Route>
-//                                     </Switch>
-//                                 </MemoryRouter>
-//                             </GuestProvider>)
-
-//     const entryInput = await screen.findByPlaceholderText('Your Entry');
-//     const button = await screen.findByRole('button', {name: 'sign'})
-
-//     userEvent.type(entryInput, 'Thanks');
-//     userEvent.click(button);
-    
-//     const nameEntry = await screen.findByText('Thanks for signing Tanner')
-//     const guestEntry = await screen.findByText('Thanks');
-
-//     expect(nameEntry).toBeInTheDocument();
-//     expect(guestEntry).toBeInTheDocument();
-//     expect(container).toMatchSnapshot();
-
-// });
 it('should render the body component', async() => {
     const { container } = render(
                             <GuestProvider>
-                                <Body />
+                                <MemoryRouter>
+                                    <Switch>
+                                        <Route exact path='/'>
+                                            <Body />
+                                        </Route>
+                                    </Switch>
+                                </MemoryRouter>
                             </GuestProvider>)
 
+    const entryInput = await screen.findByPlaceholderText('Your Entry');
+    const button = await screen.findByRole('button', {name: 'sign'})
+
+    userEvent.type(entryInput, 'Thanks');
+    userEvent.click(button);
+    
+    const nameEntry = await screen.findByText('Thanks for signing tanner')
+    const guestEntry = await screen.findByText('Thanks');
+
+    expect(nameEntry).toBeInTheDocument();
+    expect(guestEntry).toBeInTheDocument();
     expect(container).toMatchSnapshot();
+
 });
